5674fb7fbcb17778779ddb0b7ef14af75cacf8a9
mauricioiserhard <m.iserhard@hotmail.com>
2022-09-13 15:50:59 -0300

    Primeiro commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'PABC',
+                fotos: [
+                    {url:'38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrana da Silva',
+                partido: 'PDEF',
+                fotos: [
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'PJKL',
+                vice: 'João Ninguém',
+                fotos: [
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99vice.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zé Mané',
+                partido:'PQWERT',
+                vice:'Maria das Dores',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84vice.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
